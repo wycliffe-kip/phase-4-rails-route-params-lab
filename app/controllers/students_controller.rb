c7b90806aa46a44bfe322cf22b96132889e59b28
Wycliffe <wycliffekiprono4@gmail.com>
2023-06-20 10:37:03 +0300

    params

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,23 @@
 class StudentsController < ApplicationController
 
-  def index
+def index
+  if params[:query].present?
+    students = Student.where("first_name ILIKE :query OR last_name ILIKE :query", query: "%#{params[:query]}%")
+  else
     students = Student.all
-    render json: students
   end
+  render json: students
+end
+
+  
+  def grades
+    grades = Student.order(grade: :desc)
+    render json: grades
+  end 
+
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student 
+  end 
 
 end
